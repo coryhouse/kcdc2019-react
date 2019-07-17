7c995741e438cf3a1eefc44f5122934c0571607b
Cory House <housecor@gmail.com>
2019-07-17 15:09:10 -0500

    get delete workin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 
 function App() {
-  const [sessions, setSessions] = useState(["React", "C#", "Python"]);
+  const [sessions, setSessions] = useState([
+    { id: 1, title: "React" },
+    { id: 2, title: "C#" },
+    { id: 3, title: "Python" }
+  ]);
+
+  function deleteSession(id) {
+    const newSessions = sessions.filter(session => session.id !== id);
+    setSessions(newSessions);
+  }
 
   function renderSession(session) {
-    return <li key={session}>{session}</li>;
+    return (
+      <li key={session.id}>
+        <button onClick={() => deleteSession(session.id)}>Delete</button>{" "}
+        {session.title}
+      </li>
+    );
   }
 
   // what I return here gets rendered.
